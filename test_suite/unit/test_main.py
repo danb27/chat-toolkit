7ff949a8dffa74e3b014414f17964d5c2b6b834e
danb27 <danbianchini@outlook.com>
2023-03-08 02:15:33 -0800

    improve test coverage

---
+++ test_suite/unit/test_main.py
@@ -0,0 +1,33 @@
+from unittest.mock import Mock
+
+import pytest
+
+from chat_toolkit.__main__ import main, COMPONENTS
+
+
+@pytest.mark.parametrize(
+    "chatbot_model",
+    COMPONENTS["chatbot"].keys(),
+)
+@pytest.mark.parametrize(
+    "speech_to_text_model",
+    list(COMPONENTS["speech_to_text"].keys()) + [None],
+)
+def test_easy_terminal_session_imports(
+        chatbot_model: str,
+        speech_to_text_model: str,
+        monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """
+    Test that errors are not raised when setting up the orchestrator
+    dynamically.
+    """
+    monkeypatch.setattr(
+        "chat_toolkit.orchestrators.orchestrator_base.OrchestratorBase"
+        ".terminal_conversation",
+        Mock(),
+    )
+    try:
+        main(chatbot_model, speech_to_text_model)
+    except ImportError as e:
+        raise AssertionError from e
--- test_suite/unit/test_speech_to_text_conversation.py
+++ test_suite/unit/test_speech_to_text_conversation.py
@@ -46,3 +46,33 @@ def test_init(
     )
     assert isinstance(orchestrator._chatbot_component, ChatbotComponentBase)
     assert len(orchestrator.components) == 2
+
+
+@pytest.mark.parametrize("chatbot_model", CHATBOT_MODEL_TYPES)
+@pytest.mark.parametrize("speech_to_text_model", SPEECH_TO_TEXT_MODEL_TYPES)
+def test_keyboard_error_swallowed(
+    patched_openai_chatbot_factory: OpenAIChatbotFactoryType,
+    patched_openai_speech_to_text_factory: OpenAISpeechToTextFactoryType,
+    chatbot_model: str,
+    speech_to_text_model: str,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """
+    Test that terminal conversation correctly swallows a user's
+    KeyboardInterrupt.
+    """
+    chatbot = patched_openai_chatbot_factory(chatbot_model)
+    speech_to_text = patched_openai_speech_to_text_factory(
+        speech_to_text_model
+    )
+    orchestrator = SpeechToTextOrchestrator(chatbot, speech_to_text)
+
+    monkeypatch.setattr(
+        orchestrator,
+        "_terminal_conversation",
+        Mock(side_effect=KeyboardInterrupt()),
+    )
+    try:
+        orchestrator.terminal_conversation()
+    except KeyboardInterrupt as e:
+        raise AssertionError from e
