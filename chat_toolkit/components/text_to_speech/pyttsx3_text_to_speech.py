8ce1c271a4f6bf98b7e4ca1568c588ce27def615
danb27 <danbianchini@outlook.com>
2023-03-21 03:39:41 -0700

    change default speaking speed

--- chat_toolkit/components/text_to_speech/pyttsx3_text_to_speech.py
+++ chat_toolkit/components/text_to_speech/pyttsx3_text_to_speech.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pyttsx3
 
 from chat_toolkit.components.text_to_speech.text_to_speech_component_base import (  # noqa E501
@@ -10,9 +12,19 @@ class Pyttsx3TextToSpeech(TextToSpeechComponentBase):
     Class for interacting with pyttsx, a free, offline text to speech package.
     """
 
-    def __init__(self):
+    def __init__(self, speaking_rate: int = 175):
         super().__init__(pricing_rate=0.0)
         self.engine = pyttsx3.init()
+        self.set_pyttsx3_property("rate", speaking_rate)
+
+    def set_pyttsx3_property(self, pyttsx3_property: str, value: Any) -> None:
+        """
+        Set/update a property in the pyttsx3 engine. See their documentation
+        for more information.
+        :param pyttsx3_property: property to alter
+        :param value: value to set
+        """
+        self.engine.setProperty(pyttsx3_property, value)
 
     def say(self, text: str) -> dict:
         """
